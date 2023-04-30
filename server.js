16078eb9070d840fd8f53943f84a696d967909c5
gandham-jagadeesh <gandhamphotos@gmail.com>
2023-04-30 23:32:37 +0530

    port

---
+++ server.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const cors    = require("cors");
+const app     = express()
+const mongoose = require("mongoose");
+const cookieParser   = require('cookie-parser');
+const multer  = require('multer')
+const uploadMiddleware = multer({ dest: 'uploads/'})
+const port    = process.env.PORT;
+
+app.use("/uploads",express.static(__dirname+'/uploads'));
+app.use(cors());
+app.use(express.json())
+app.use(cookieParser());
+
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.mongo);
+
+app.use("/register",require("./controllers/register"));
+app.use("/login",require("./controllers/login"));
+app.use("/profile",require("./controllers/profile"));
+app.use("/logout",require("./controllers/logout"));
+app.use("/post",uploadMiddleware.single('file'),require("./controllers/post"));
+
+app.listen(port,()=>{
+    console.log(`${port}`);
+})
